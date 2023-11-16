fc005d299388903338c7891ecee7e99c60d54b5e
GennadyBr <gennady.budazhapov@gmail.com>
2023-11-16 15:33:33 +0300

    out_branch_op

--- dags/movie_etl.py
+++ dags/movie_etl.py
@@ -189,11 +189,9 @@ with DAG(
 
 init >> task_validate_params >> in_branch_op
 
-in_branch_op >> task_pg_get_movies_ids >> task_pg_get_films_data
-task_pg_get_films_data >> out_branch_op
+in_branch_op >> task_pg_get_movies_ids >> task_pg_get_films_data >> out_branch_op
 
-in_branch_op >> task_sqlite_get_movies_ids >> task_sqlite_get_films_data
-task_sqlite_get_films_data >> out_branch_op
+in_branch_op >> task_sqlite_get_movies_ids >> task_sqlite_get_films_data >> out_branch_op
 
 out_branch_op >> task_es_preprocess >> task_es_create_index >> task_es_write >> final
 
