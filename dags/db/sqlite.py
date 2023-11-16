305fc21bd042560cf9275302524146dc3e7d761b
GennadyBr <gennady.budazhapov@gmail.com>
2023-11-16 19:39:47 +0300

    def sqlite_get_films_data(ti: TaskInstance, **context):

--- dags/db/sqlite.py
+++ dags/db/sqlite.py
@@ -8,7 +8,7 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.providers.sqlite.hooks.sqlite import SqliteHook
 from psycopg2.extras import RealDictCursor
 
-from settings import DBFileds, SQLiteDBTables, MOVIES_UPDATED_STATE_KEY
+from settings import DBFileds, SQLiteDBTables, MOVIES_UPDATED_STATE_KEY, FILEDS2SQL
 
 
 def sqlite_get_updated_movies_ids(ti: TaskInstance, **context):
@@ -32,17 +32,12 @@ def sqlite_get_updated_movies_ids(ti: TaskInstance, **context):
     logging.info(msg)
 
     sqlite_hook = SqliteHook(sqlite_conn_id=context["params"]["in_db_id"])
-    msg = f"sqlite_hook = {str(sqlite_hook)}, {str(type(sqlite_hook))}"
-    logging.info(msg)
     sqlite_con = sqlite_hook.get_conn()
     sqlite_cur = sqlite_con.cursor()
-    msg = f"sqlite_cur = {str(sqlite_cur)}, {str(type(sqlite_cur))}"
-    logging.info(msg)
-
     sqlite_cur.execute(query, (updated_state_sqlite,))
     sqlite_tuples_list = sqlite_cur.fetchall()
     sqlite_dict_list = [dict(zip(['id', 'updated_at'], tuple)) for tuple in sqlite_tuples_list]
-
+    sqlite_con.close()
     msg = f"sqlite_items = {str(sqlite_dict_list)}, {str(type(sqlite_dict_list))}"
     logging.info(f'SQLITE_CURSOR SUCCESS;= {msg}')
 
@@ -53,12 +48,45 @@ def sqlite_get_updated_movies_ids(ti: TaskInstance, **context):
 
 def sqlite_get_films_data(ti: TaskInstance, **context):
     """Сбор агрегированных данных по фильмам"""
-    logging.info(f'def sqlite_get_films_data= {ti}, {context}')
+    logging.info(context["params"]["fields"])
+    fields_query = ", ".join([FILEDS2SQL[field] for field in context["params"]["fields"]])
+
+    query = f"""
+        SELECT {fields_query}
+        FROM {SQLiteDBTables.film.value} fw
+        LEFT JOIN {SQLiteDBTables.film_person.value} pfw ON pfw.film_work_id = fw.id
+        LEFT JOIN {SQLiteDBTables.person.value} p ON p.id = pfw.person_id
+        LEFT JOIN {SQLiteDBTables.film_genre.value} gfw ON gfw.film_work_id = fw.id
+        LEFT JOIN {SQLiteDBTables.genre.value} g ON g.id = gfw.genre_id
+        WHERE fw.id IN ?
+        GROUP BY fw.id;
+        """
+
+    film_ids = ti.xcom_pull(task_ids="sqlite_get_updated_movies_ids")
+    if len(film_ids) == 0:
+        logging.info("No records need to be updated")
+        return
+
+    sqlite_hook = SqliteHook(sqlite_conn_id=context["params"]["in_db_id"])
+    sqlite_con = sqlite_hook.get_conn()
+    sqlite_cur = sqlite_con.cursor()
+    sqlite_cur.execute(query, (tuple(film_ids),))
+    sqlite_tuples_list = sqlite_cur.fetchall()
+    msg = f"sqlite_tuples_list = {sqlite_tuples_list}, {type(sqlite_tuples_list)}"
+    logging.info(f'SQLITE_CURSOR SUCCESS;= {msg}')
+
+    sqlite_dict_list = [dict(zip(['id', 'updated_at'], tuple)) for tuple in sqlite_tuples_list]
+    sqlite_con.close()
+    msg = f"sqlite_dict_list = {sqlite_dict_list}, {type(sqlite_dict_list)}"
+    logging.info(f'SQLITE_CURSOR SUCCESS;= {msg}')
+
+    return json.dumps(sqlite_dict_list, indent=4)
+
 
 def sqlite_preprocess(ti: TaskInstance, **context):
     """Трансформация данных"""
-    logging.info(f'def sqlite_preprocess= {ti}, {context}')
+    logging.info(f'def sqlite_preprocess= {ti.xcom_pull(task_ids="sqlite_get_films_data")}, context= {context["params"]}')
 
 def sqlite_write(ti: TaskInstance, **context):
     """Запись данных"""
-    logging.info(f'sqlite_write= {ti}, {context}')
+    logging.info(f'sqlite_write= {ti.xcom_pull(task_ids="sqlite_preprocess")}, context= {context["params"]}')
