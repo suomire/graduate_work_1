bd7e6d3168784d1a7736c50f017fdebd525a7e1d
GennadyBr <gennady.budazhapov@gmail.com>
2023-11-17 15:04:54 +0300

    cursor = conn.cursor()         # cursor.execute(""".table""")

--- dags/db/sqlite.py
+++ dags/db/sqlite.py
@@ -54,8 +54,9 @@ def sqlite_get_updated_movies_ids(ti: TaskInstance, **context):
     db_name = BaseHook.get_connection(context["params"]["in_db_id"]).schema
     logging.info(f"{db_name=}")
 
-    with conn_context(db_name) as conn, conn.cursor() as cursor:
-        cursor.execute(""".table""")
+    with conn_context(db_name) as conn:
+        cursor = conn.cursor()
+        # cursor.execute(""".table""")
         cursor.execute("""select * from film_work""")
         sqlite_dict_list = cursor.fetchall()
         logging.info(f'{sqlite_dict_list=}')
